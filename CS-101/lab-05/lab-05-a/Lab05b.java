7a9e63ccdc7b04a4a27d21f0e99a9adedfc802ff
AliSalemwala <ali.salemwala@gmail.com>
2019-12-23 02:34:35 +0300

    Added CS 101 lab 02

---
+++ CS-101/lab-05/lab-05-a/Lab05b.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/** Lab05a task
+  * make walls with holes using loops
+  * Author - Ali Altaf Salemwala
+  * 3rd November, 2015
+  */
+
+public class Lab05a
+{
+  public static void main (String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+   in.close();
+  }
+}
